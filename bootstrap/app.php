f1c1a139d7b46f80553c21dc7a81c882e7979d90
Therixxx23 <rifqiariqf@gmail.com>
2026-06-03 01:27:33 +0700

    fix: include vite build assets for production

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin.auth' => \App\Http\Middleware\AdminAuth::class,
         ]);
+
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
